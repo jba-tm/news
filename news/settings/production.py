c3d3cc85a57f34925c1b2702c23eb6e05edb473f
jba-tm <66518710+jba-tm@users.noreply.github.com>
2020-11-02 17:39:13 +0500

    Deploy to pythonanywhere

--- news/settings/production.py
+++ news/settings/production.py
@@ -4,29 +4,41 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-SECRET_KEY = 'g&#y4)nawrii92s%=(hgwrb2&ph&k1n2kmu=i75t_737__=j-1'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 WAGTAIL_SITE_NAME = _('World news')
 
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 # Session settings
-# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 #
 # # Publish settings
 SECURE_SSL_REDIRECT = False
 #
-# SECURE_HSTS_SECONDS = 3600
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-#
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-#
-# SECURE_BROWSER_XSS_FILTER = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
-# CSRF_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 
-# X_FRAME_OPTIONS = "DENY"
-#
-# SECURE_REFERRER_POLICY = 'origin'
+SECURE_BROWSER_XSS_FILTER = True
+
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = "DENY"
+
+SECURE_REFERRER_POLICY = 'origin'
 
 try:
     from .local import *
